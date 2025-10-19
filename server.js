8748309d01632df9449a772e156d02e9bde7536b
Eh-Ho <ehosseini8001.work@gmail.com>
2025-10-19 19:01:40 +0330

    server listener created

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server = http.createServer((req,res)=>{
+
+    res.write('hello world')    
+    res.end();
+})
+
+server.listen(3000,()=>{
+    console.log("Server is running on port 3000");
+});
