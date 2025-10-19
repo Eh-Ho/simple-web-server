95f3b0a23465adc5362e5d61e18c4a4dd1fa24c9
Eh-Ho <ehosseini8001.work@gmail.com>
2025-10-20 00:35:08 +0330

    file sync bug fixed

--- server.js
+++ server.js
@@ -6,22 +6,31 @@ const mime = require('mime-types');
 
 const server = http.createServer((req,res)=>{
     filepath = path.join(process.cwd(),'web',req.url);
+    let file_status
 
     try{
-        fs.lstatSync(filepath)
-        let data = fs.readFileSync(filepath)
+        file_status = fs.lstatSync(filepath)
+    }catch(err){
+        res.writeHead(404,{'Content-Type':'text/html'});
+        res.write(fs.readFileSync(path.join(process.cwd(),'web','404.html')));
+        return res.end()
+    }
 
+    if(file_status.isFile()){
+        data = fs.readFileSync(filepath)
         type = mime.contentType(path.extname(filepath))
         res.writeHead(200,{'Content-Type':type});
         res.write(data)
-
-    }catch(err){
-        console.log(err)
-        res.end();
+        res.end()
+    }else if(!file_status.isFile()){
+        res.writeHead(302,{'location':req.url + 'index.html'})
+        res.end()   
+    }else{
+        res.writeHead(500,{'content-type':'text/html'})
+        res.write(fs.readFileSync(path.join(process.cwd(),'web','500.html')))
+        res.end()
     }
-
-    res.end()
-
+    
 })
 
 
