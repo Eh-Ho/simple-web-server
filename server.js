3cbb846be0931a7c43c3779df95ec3a5785341e3
Eh-Ho <ehosseini8001.work@gmail.com>
2025-10-19 22:56:13 +0330

    loading index.html

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const http = require('http');
+const fs = require('fs');
+const process = require('process');
+const path = require('path');
 
 const server = http.createServer((req,res)=>{
+    filepath = path.join(process.cwd(),'web',req.url);
 
-    res.write('hello world')    
-    res.end();
+    try{
+
+        fs.lstatSync(filepath)
+        let data = fs.readFileSync(filepath)
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write(data)
+
+    }catch(err){
+        console.log(err)
+        res.end();
+    }
+
+    res.end()
+
 })
+
 
 server.listen(3000,()=>{
     console.log("Server is running on port 3000");
