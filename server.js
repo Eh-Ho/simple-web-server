30a1ffadb4d7a0c0b5472cdd4796aa2d5370cc99
Eh-Ho <ehosseini8001.work@gmail.com>
2025-10-19 23:13:42 +0330

    successfully loaded the website

--- server.js
+++ server.js
@@ -2,15 +2,17 @@ const http = require('http');
 const fs = require('fs');
 const process = require('process');
 const path = require('path');
+const mime = require('mime-types');
 
 const server = http.createServer((req,res)=>{
     filepath = path.join(process.cwd(),'web',req.url);
 
     try{
-
         fs.lstatSync(filepath)
         let data = fs.readFileSync(filepath)
-        res.writeHead(200,{'Content-Type':'text/html'});
+
+        type = mime.contentType(path.extname(filepath))
+        res.writeHead(200,{'Content-Type':type});
         res.write(data)
 
     }catch(err){
